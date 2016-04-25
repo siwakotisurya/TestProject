1cb96bf25da73497f631c361de37593cfbbfe1c5
Surya Siwakoti <suryasiwakoti@Suryas-MacBook-Pro.local>
2016-04-25 16:53:34 +0545

    added two prototype for header and body and image view added

---
+++ MyProject/LogoHeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LogoHeaderTableViewCell.swift
+//  MyProject
+//
+//  Created by Surya Siwakoti on 4/25/16.
+//  Copyright © 2016 Surya Siwakoti. All rights reserved.
+//
+
+import UIKit
+
+class LogoHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MyProject/ViewController.swift
+++ MyProject/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var detailTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +21,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func 
 
 
 }
